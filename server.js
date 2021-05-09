3c1c92bc9cd6c03b91d1d20f737605c3acebb77b
Neekit <neekit-p@valuepitch.com>
2021-05-09 12:25:10 +0530

    pkg file changed

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ mongoose.connect(process.env.db_url, opts).then(() => {
 });
 
 
-let port = process.env.port || "3000";
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`connected to PORT ${process.env.port}`)
 })
